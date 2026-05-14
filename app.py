693b0779e8c3d5bd523fd47b697b5bb501946351
farzeena <fathimafarzeena960@gmail.com>
2026-05-14 11:09:38 +0530

    Added submit popup

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def home():
 
             <input type="text" placeholder="Enter your name"><br>
 
-            <button>Submit</button>
+            <button><button onclick="alert('Form Submitted Successfully!')">Submit</button></button>
         </div>
     </body>
     </html>
